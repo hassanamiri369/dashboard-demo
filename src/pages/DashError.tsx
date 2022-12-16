61b5afaca4ebfcdd3883236c958727e9e1f3aaac
Hassanamiri <hassanamiri369@gmail.com>
2022-12-16 15:54:50 +0330

    create setting , user and dashError components

---
+++ src/pages/DashError.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Link , useRouteError} from "react-router-dom";
+
+  
+const DashError = () => {
+    const error = useRouteError () as any;
+
+
+  return (
+ <>
+    <div>
+        <h1>Oops! something went wrong</h1>
+        <p>
+            <strong>
+                Error : {error.statusText} {error.status}
+            </strong>
+         <div>
+         {/* <Link to={'/'} className="text-blue-600 underline">Back to home</Link> */}
+         </div>
+        </p>
+    </div>
+ </>
+  )
+}
+
+export default DashError
