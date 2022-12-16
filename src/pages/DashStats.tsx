e0117ad235e08582d985f7e0d5989b976781a079
Hassanamiri <hassanamiri369@gmail.com>
2022-12-17 02:05:56 +0330

    show user data

--- src/pages/DashStats.tsx
+++ src/pages/DashStats.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import {useLoaderData} from "react-router-dom"
-import { PostType } from '../typing'
+import {useContext} from 'react'
+// import {useLoaderData} from "react-router-dom"
+// import { PostType } from '../typing'
+import { TaskContext } from '../context/PostContext'
 
 const DashStats = () => {
 
     // get all data
-    const {posts} = useLoaderData()  as {posts : PostType[]}
+    // const {posts} = useLoaderData()  as {posts : PostType[]}
+
+    const context = useContext(TaskContext)
+    const {state} = context;
+    
+
+
+
     
     
     return (
         <div>
             <h1>Dashboard Stats</h1>
-            <p>there are {posts.length} posts</p>
+            <p>there are <span className='font-bold'>{state.tasks.length}</span> posts</p>
+            {/* user list */}
+            <div>
+                <p>count user : <span  className='font-bold'>{state.userList.length}</span></p>
+            </div>
         </div>
     )
 }
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { TaskContext } from '../context/PostContext'
 
 const Users = () => {
+
+    const context = useContext(TaskContext)
+    const {state} = context;
     return (
         <div>
-            users
+            <h1>Users</h1>
+            <div>{state.userList  && <div>{state.userList.map((user)=><div>
+                <ul className='p-4 border m-2'>
+                    <li>{user.name}</li>
+                    <li>{user.email}</li>
+                    <li>{user.password}</li>
+                </ul>
+            </div>)}</div>}</div>
         </div>
     )
 }
