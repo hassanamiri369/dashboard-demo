71a9bfd632d1a646645133f6c1776bccbed3c529
Hassanamiri <hassanamiri369@gmail.com>
2022-12-16 10:41:32 +0330

    add router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 
+import {createBrowserRouter , RouterProvider} from "react-router-dom"
+
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Dashboard from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {path : "/" ,
+  element : <Home/>
+  },
+  {path : "/about" , 
+    element : <About/>
+  },
+  {path : "/contact"
+  , element : <Contact/>
+},
+{path : "/dashboard" ,
+  element : <Dashboard/>
+}
+])
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <RouterProvider router={router}/>
+
      
-    </div>
+    </>
   );
 }
 
