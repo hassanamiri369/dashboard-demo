95cb8966b8fe822184105698874156bc4228380a
Hassanamiri <hassanamiri369@gmail.com>
2022-12-16 11:06:34 +0330

    delete route this file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 
-import {createBrowserRouter , RouterProvider} from "react-router-dom"
+import { RouterProvider} from "react-router-dom"
 
-import About from "./pages/About";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Dashboard from "./pages/Dashboard";
+// route page 
+import {router} from "./router/index"
 
-const router = createBrowserRouter([
-  {path : "/" ,
-  element : <Home/>
-  },
-  {path : "/about" , 
-    element : <About/>
-  },
-  {path : "/contact"
-  , element : <Contact/>
-},
-{path : "/dashboard" ,
-  element : <Dashboard/>
-}
-])
+
 
 function App() {
   return (
