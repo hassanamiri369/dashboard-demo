bb35ce155ef20268c6a77aded1c13e9586863e72
Hassanamiri <hassanamiri369@gmail.com>
2022-12-16 23:52:31 +0330

    create detail post page for show detail post

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import {useLoaderData} from "react-router-dom"
-import { PostType } from '../typing'
+import React , {useContext , useState,useEffect} from 'react'
+import { ITasks, TaskContext } from '../context/PostContext'
+
+
+import { Link, useLocation, useParams } from 'react-router-dom'
 
 const Post = () => {
-    const {post} = useLoaderData() as {post : PostType | undefined};
 
-    console.log(post)
+    const context = useContext(TaskContext)
+  const posts = context.state.tasks;
+    
+  const [postDetail , setPostDetail] = useState<ITasks | null>(null)
+    
+    const {id} = useParams()
+    console.log(id)
+
+
+    React.useEffect(()=>{
+        const findPost = posts.find(item => item.id === Number(id))
+        console.log(findPost)
+        if(findPost){
+            setPostDetail(findPost)
+        }
+    } , [Number(id)])
+
     return (
         <div>
-            {post ? <div className='p-4'>
-                <h2 className='font-bold'>Email : {post.email}</h2>
-                <p >Body : {post.body}</p>
-                <p>post ID : {post.postId}</p>
-            </div> : <div>Loading ...</div>}
+            {postDetail && <div>
+                <ul>
+                    <li>  <Link className='border' to={"/"}>Go Back</Link></li>
+                    <li>Post ID : {postDetail.id}</li>
+                    <li>Title : {postDetail.title}</li>
+                    <li>Body : {postDetail.body}</li>
+
+                </ul>
+                </div>}
         </div>
     )
 }
