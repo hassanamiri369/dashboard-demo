9fc94b9297b24d8039ce9b887d1bb538ec8b74be
Hassanamiri <hassanamiri369@gmail.com>
2022-12-16 23:47:49 +0330

    added taskcontext provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import TaskContextProvider from './context/PostContext';
 
 
 const root = ReactDOM.createRoot(
@@ -9,6 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <TaskContextProvider>
+
     <App />
+    </TaskContextProvider>
   </React.StrictMode>
 );
