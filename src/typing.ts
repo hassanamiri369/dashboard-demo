2685596c3a92044dda0c4302c855bb500bac8e09
Hassanamiri <hassanamiri369@gmail.com>
2022-12-16 15:54:03 +0330

    added post type

--- src/typing.ts
+++ src/typing.ts
@@ -3,3 +3,11 @@ export type UserType ={
     id : string | number;
     email : string
 }
+
+
+export type PostType = {
+    postId : number;
+    id : number | string;
+    email : string;
+    body : string
+}
